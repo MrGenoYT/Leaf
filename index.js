ec1920b718068acf7650553e427d1358b029eb16
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-03-09 00:42:26 +0530

    index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function moveRandomly() {
 }
 
 // Express for web hosting on Render
+const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
